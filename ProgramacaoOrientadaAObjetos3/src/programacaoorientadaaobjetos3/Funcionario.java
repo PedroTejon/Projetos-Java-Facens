db94ad860a91b1c085bdab55c3ce233bad0c98a6
PedroTejon <pedrohtejon@gmail.com>
2023-09-11 20:28:41 -0300

    11/09/2023 - 1/2

---
+++ ProgramacaoOrientadaAObjetos3/src/programacaoorientadaaobjetos3/Funcionario.java
@@ -0,0 +1,64 @@
+package programacaoorientadaaobjetos3;
+
+public class Funcionario {
+    private int codigo;
+    private String nome;
+    private String setor;
+    private String funcao;
+    private double salario;
+    
+    public void Funcionario() {        
+    }
+    
+    public void Funcionario(int codigo, String nome, String setor, String funcao, double  salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.setor = setor;
+        this.funcao = funcao;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public String imprimir() {
+        return "Código: %d\nNome: %s\nSetor: %s\nFunção: %s\nSalário: %.2f".formatted(codigo, nome, setor, funcao, salario);
+    }
+}
